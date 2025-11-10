c9e3323c19ea44524fa62efb5f5383d71a342742
Sehabulislam <shehabulislam244@gmail.com>
2025-11-11 01:43:17 +0600

    added the product section

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
@@ -8,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://smartDBUser:NODWkeEd9GMTIaas@cluster0.cjsthkf.mongodb.net/?appName=Cluster0";
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjsthkf.mongodb.net/?appName=Cluster0`
+
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -112,6 +113,17 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    // app.get('/bids',async(req,res)=>{
+    //   const query = {};
+    //   if(query.email){
+    //     query.buyer_email = email;
+    //   }
+    //   const cursor = bidsCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
+
     app.get("/products/bids/:productId", async (req, res) => {
       const productId = req.params.productId;
       const query = { product: productId };
@@ -119,16 +131,6 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
-    app.get('/bids',async(req,res)=>{
-      const query = {};
-      if(query.email){
-        query.buyer_email = email;
-      }
-      const cursor = bidsCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
     app.post("/bids", async (req, res) => {
       const newBid = req.body;
       const result = await bidsCollection.insertOne(newBid);
@@ -137,7 +139,7 @@ async function run() {
 
     app.delete("/bids/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: id };
+      const query = { _id: new ObjectId(id) };
       const result = await bidsCollection.deleteOne(query);
       res.send(result);
     });
